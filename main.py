635cc809ba0f6e6aeaf43700679a1fa8d1fe1196
beckm33 <beckm@gilmour.org>
2017-09-20 14:44:13 -0400

    pulse, cruise, sweep added

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import gopigo
 import time
 
@@ -9,38 +8,38 @@ class Piggy(object):
         print("I AM ALIVE")
 
 
-def pulse(self):
-    """check for obstacles, drive fixed amount forward"""
-    look = us_dist(15) # store the distance reading
-    if look > 80:
+    def pulse(self):
+        """check for obstacles, drive fixed amount forward"""
+        look = us_dist(15) # store the distance reading
+        if look > 80:
+            fwd()
+            time.sleep(1)
+            stop()
+
+
+
+    def cruise(self):
+        """drive fwd, stop if sensor detects obstacle"""
         fwd()
-        time.sleep(1)
-        stop()
+        while(True):
+            if us_dist(15) < 30:
+                stop()
+            time.sleep(.2)
 
 
+    def servo_sweep(self):
+        """loops in a 120 degree arch and moves servo"""
+        for ang in range(20, 160, 2):
+            servo(ang)
+            time.sleep(.2)
 
-def cruise(self):
-    """drive fwd, stop if sensor detects obstacle"""
-    fwd()
-    while(True):
-        if us_dist(15) < 30:
-            stop()
-        time.sleep(.2)
-
-
-def servo_sweep(self):
-    """loops in a 120 degree arch and moves servo"""
-    for ang in range(20, 160, 2):
-        servo(ang)
-        time.sleep(.2)
-
-    def cha_cha(self):
-        for x in range(5):
-            right_rot()
-            time_sleep(.5)
-            left_rot()
-            time.sleep(.5)
-            stop()
+        def cha_cha(self):
+            for x in range(5):
+                right_rot()
+                time_sleep(.5)
+                left_rot()
+                time.sleep(.5)
+                stop()
 
 
 pedro = Piggy()
